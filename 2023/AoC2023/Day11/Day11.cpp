546c678feba932e93d1fffdf03f5741dc6b87c17
egnicholasdibb-fuller <nicholasdibb-fuller@egtechnology.co.uk>
2023-12-11 11:33:43 +0000

    solved day 11 part 2

--- 2023/AoC2023/Day11/Day11.cpp
+++ 2023/AoC2023/Day11/Day11.cpp
@@ -8,9 +8,11 @@
 
 typedef std::vector<std::string> Matrix;
 
-typedef struct
+typedef struct coord_t
 {
     uint16_t id, x, y;
+    uint16_t x_exp = 0;
+    uint16_t y_exp = 0;
 } CoOrd;
 
 Matrix createUniverse(std::vector<CoOrd>& galaxies)
@@ -19,6 +21,10 @@ Matrix createUniverse(std::vector<CoOrd>& galaxies)
     std::string line;
     std::ifstream input("input.txt");
 
+    std::vector<uint16_t> horizontal_empty;
+    std::vector<uint16_t> vertical_empty;
+
+    uint16_t line_num = 0;
     while (std::getline(input, line)) // get input from file
     {
         bool empty = true;
@@ -33,10 +39,11 @@ Matrix createUniverse(std::vector<CoOrd>& galaxies)
 
         if (empty) // add two for each empty line
         {
-            universe.push_back(line);
+            horizontal_empty.push_back(line_num);
         }
 
         universe.push_back(line);
+        line_num++;
     }
 
     for (int i = 0; i < universe[0].length(); i++) // iterate through each column
@@ -52,11 +59,7 @@ Matrix createUniverse(std::vector<CoOrd>& galaxies)
 
         if (empty)
         {
-            for (int j = 0; j < universe.size(); j++) // iterate down the rows
-            {
-                universe[j].insert(i, "."); // add a new value for each one and increment the ticker for i
-            }
-            i++;
+            vertical_empty.push_back(i);
         }
     }
 
@@ -72,6 +75,19 @@ Matrix createUniverse(std::vector<CoOrd>& galaxies)
                 galaxy.id = galaxy_num++;
                 galaxy.x = x;
                 galaxy.y = y;
+
+                for (auto v : vertical_empty)
+                {
+                    if (v < galaxy.x)
+                        galaxy.y_exp++;
+                }
+
+                for (auto h : horizontal_empty)
+                {
+                    if (h < galaxy.y)
+                        galaxy.x_exp++;
+                }
+
                 galaxies.push_back(galaxy);
             }
         }
@@ -80,20 +96,9 @@ Matrix createUniverse(std::vector<CoOrd>& galaxies)
     return universe;
 }
 
-void printMatrix(Matrix m)
+uint64_t countSteps(std::vector<CoOrd> galaxies, uint32_t expansion_coefficient = 2)
 {
-    for (int i = 0; i < m.size(); i++)
-    {
-        printf("%s\n", m[i].c_str());
-    }
-}
-
-void Part1(void)
-{
-    std::vector<CoOrd> galaxies;
-    Matrix universe = createUniverse(galaxies);
-
-    uint32_t total = 0;
+    uint64_t total = 0;
 
     for (auto g : galaxies)
     {
@@ -108,15 +113,44 @@ void Part1(void)
             int16_t y_diff = g.y - h.y;
             if (y_diff < 0)
                 y_diff *= -1;
+            int16_t x_exp_diff = g.x_exp - h.x_exp;
+            if (x_exp_diff < 0)
+                x_exp_diff *= -1;
+            int16_t y_exp_diff = g.y_exp - h.y_exp;
+            if (y_exp_diff < 0)
+                y_exp_diff *= -1;
 
-            total += x_diff + y_diff;
+            /*printf("id: %d\tx: %d\ty: %d\tx_expan: %d\ty_expan: %d\nid: %d\tx: %d\ty: %d\tx_expan: %d\ty_expan: %d\nx_diff: %d\ty_diff: %d\tx_exp_diff: %d\ty_exp_diff: %d\nsteps: %d\n\n",
+                h.id, h.x, h.y, h.x_exp, h.y_exp,
+                g.id, g.x, g.y, g.x_exp, g.y_exp,
+                x_diff, y_diff, x_exp_diff, y_exp_diff,
+                x_diff + y_diff + x_exp_diff + y_exp_diff);*/
+
+            total += x_diff + y_diff + (expansion_coefficient-1) * (x_exp_diff + y_exp_diff);
         }
     }
 
-    printf("the total for part 1 is:%d\n", total);
+    return total;
+}
+
+void Part1(void)
+{
+    std::vector<CoOrd> galaxies;
+    Matrix universe = createUniverse(galaxies);
+
+    printf("the total for part 1 is:\t%llu\n", countSteps(galaxies));
+}
+
+void Part2(void)
+{
+    std::vector<CoOrd> galaxies;
+    Matrix universe = createUniverse(galaxies);
+
+    printf("the total for part 2 is:\t%llu\n", countSteps(galaxies, 1000000));
 }
 
 int main()
 {
     Part1();
+    Part2();
 }

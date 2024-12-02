6eede4d3212a5aa9c31afdfbda28652633b39f58
nicholasdibb-fuller <nicholas.dibb-fuller@disguise.one>
2024-12-02 12:43:49 +0000

    completed day 1

--- AoC2024/Day1/Day1.cpp
+++ AoC2024/Day1/Day1.cpp
@@ -1,13 +1,74 @@
 ﻿// Day1.cpp : Defines the entry point for the application.
 //
 
-#include "Day1.h"
+#include <algorithm>
 #include <iostream>
+#include <list>
+#include <sstream>
+#include <stdint.h>
+
 #include "inputtaker.h"
+
+void part1()
+{
+    std::list<int32_t> leftList, rightList;
+
+	std::string input = InputTaker::readAll();
+
+	while (InputTaker::get().moreToRead())
+	{
+	    std::string line = InputTaker::get().readLine();
+
+		const size_t leftSize = line.find(' ');
+		const size_t rightSize = line.find('\n') - (leftSize + 3);
+
+		leftList.emplace_back(stoi(line.substr(0, leftSize)));
+		rightList.emplace_back(stoi(line.substr(leftSize + 3, rightSize)));
+	}
+
+
+	leftList.sort();
+	rightList.sort();
+
+	int64_t diffSum = 0;
+
+	for (auto leftIt = leftList.begin(), rightIt = rightList.begin(); leftIt != leftList.end(); ++leftIt, ++rightIt)
+		diffSum += std::abs(*leftIt - *rightIt);
+
+	std::cout << "sum of differences is " << diffSum << std::endl;
+
+	InputTaker::get().reset();
+}
+
+void part2()
+{
+    std::list<int32_t> leftList, rightList;
+
+	std::string input = InputTaker::readAll();
+
+	while (InputTaker::get().moreToRead())
+	{
+	    std::string line = InputTaker::get().readLine();
+
+		const size_t leftSize = line.find(' ');
+		const size_t rightSize = line.find('\n') - (leftSize + 3);
+
+		leftList.emplace_back(stoi(line.substr(0, leftSize)));
+		rightList.emplace_back(stoi(line.substr(leftSize + 3, rightSize)));
+	}
+
+	int64_t simScore = 0;
+
+	for (const auto& num : leftList)
+	    simScore += num * std::ranges::count(rightList, num);
+
+	std::cout << "similarity score is " << simScore << std::endl;
+
+}
 
 int main()
 {
-    std::cout << InputTaker::get().getAll();
-
-	return 0;
+	part1();
+	part2();
+    return 0;
 }
